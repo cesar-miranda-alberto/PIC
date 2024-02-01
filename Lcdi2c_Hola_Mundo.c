24610e3245ea270fd14f873b2f7e399164191884
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-02-01 14:20:29 -0600

    Lcdi2c_Hola_Mundo.c

--- Lcdi2c_Hola_Mundo.c
+++ Lcdi2c_Hola_Mundo.c
@@ -1,22 +1,23 @@
 /*
             ****** LCD I2C *****
           
-      El presente cÛdigo desarrollado en C tiene como finalidad la implementaciÛn del protocolo i2c a travÈs de
-      un lcd 16x2 donde se exhibir· texto de hola mundo a manera de comprobar el funcionamiento del cÛdigo.
-      El cÛdigo esta dirigido al microcontrolador PIC16F877A.
-      
+      El presente c√≥digo desarrollado en C tiene como finalidad la implementaci√≥n del protocolo i2c a trav√©s de
+      un lcd 16x2 donde se exhibir√° texto de hola mundo a manera de comprobar el funcionamiento del c√≥digo.
+      El c√≥digo esta dirigido al microcontrolador PIC16F877A.
+      Fecha: 18-Marzo-2022
+      Versi√≥n: 1.0        
 */
 
-#include <16f877a.h>                                                                      // Agrega la libreria del microcontrolador que se utilizar·
-#fuses HS,NOWDT,NOPUT,NOLVP,NOBROWNOUT,NODEBUG,NOLVP,NOPROTECT                            // Establecen los fusibles que se utilizar·n
+#include <16f877a.h>                                                                      // Agrega la libreria del microcontrolador que se utilizar√°
+#fuses HS,NOWDT,NOPUT,NOLVP,NOBROWNOUT,NODEBUG,NOLVP,NOPROTECT                            // Establecen los fusibles que se utilizar√°n
 #use delay(clock=20M)                                                                     // Establece la velocidad del reloj externo
-#use standard_io(D)                                                                       // Establece el puerto que se utilizar·
-#use I2C(MASTER, SDA=PIN_C4, SCL=PIN_C3, FAST)                                            // ConfiguraciÛn de los par·metros 
+#use standard_io(D)                                                                       // Establece el puerto que se utilizar√°
+#use I2C(MASTER, SDA=PIN_C4, SCL=PIN_C3, FAST)                                            // Configuraci√≥n de los par√°metros 
 #include <i2c_flex_LCD.c>                                                                 // Incluye la libreria del protocolo i2c
 
-#define ADDRESS_LCD 0X4E                                                                  // Configura la direcciÛn del Circuito Integrado
+#define ADDRESS_LCD 0X4E                                                                  // Configura la direcci√≥n del Circuito Integrado
 
-void main()                                                                               // FunciÛn Principal
+void main()                                                                               // Funci√≥n Principal
 {
    lcd_init(0x4E, 16, 2);
    
